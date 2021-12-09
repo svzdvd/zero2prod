952ec0e0076b48cefb1868e9c01fe3aaffa72f4a
Davide Savazzi <davide.savazzi@gmail.com>
2021-12-09 21:43:55 +0100

    postgresql configuration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -22,6 +24,11 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
@@ -36,6 +43,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
